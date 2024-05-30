5a257bbe8a5e95d87412c60cb75225eccd64280b
sajanIocod <sajan@iocod.com>
2024-05-30 13:02:57 +0530

    fix : focusNode

--- lib/src/quill_editor_wrapper.dart
+++ lib/src/quill_editor_wrapper.dart
@@ -43,6 +43,7 @@ class QuillHtmlEditor extends StatefulWidget {
     this.loadingBuilder,
     this.inputAction = InputAction.newline,
     this.autoFocus = false,
+    this.focusNode,
     this.textStyle = const TextStyle(
       fontStyle: FontStyle.normal,
       fontSize: 20.0,
@@ -122,6 +123,7 @@ class QuillHtmlEditor extends StatefulWidget {
   ///[hintTextPadding] optional style to set padding to the editor's text,
   /// default padding will be EdgeInsets.zero
   final EdgeInsets? hintTextPadding;
+  final FocusNode? focusNode;
 
   /// [ensureVisible] by default it will be set to false, set it to true to
   /// make sure the focus area of the editor is visible.
@@ -332,6 +334,7 @@ class QuillHtmlEditorState extends State<QuillHtmlEditor> {
                 callBack: (map) {
                   _hasFocus = map?.toString() == 'true';
                   if (widget.onFocusChanged != null) {
+                    FocusScope.of(context).requestFocus(widget.focusNode);
                     widget.onFocusChanged!(_hasFocus);
                   } else {
                     widget.onFocusChanged!(!_hasFocus);
